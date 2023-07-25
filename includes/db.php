fa236a8e92bf46f96611455b9dcc3970077a3307
Parreiral, Eduardo Pratas <eparreiral@deloitte.pt>
2023-07-25 15:01:15 +0100

    database

---
+++ includes/db.php
@@ -0,0 +1,17 @@
+<?php
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "lGGxoQtR4m392yj5";
+$db['db_name'] = "cmsproject";
+
+foreach ($db as $key => $value) {
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect($db['db_host'],$db['db_user'],$db['db_pass'],$db['db_name']);
+
+if($connection)
+{
+    echo "WER ARE CONNECTED";
+}
+?>
